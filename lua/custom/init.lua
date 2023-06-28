4e79c24eb1651a126ba321a3751a761ce9ab4b70
“Ricardo <ricardo.ribeiro@achievers.com>
2023-06-28 17:14:46 -0400

    hightlight on yanking

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -10,4 +10,14 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Replace all occurrences of a selected text at once
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
 
