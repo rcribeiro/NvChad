2a54d51b2419693635ad892e5f31321fcfdb5e46
Ricardo Ribeiro <ricardo.ribeiro@assure.co>
2024-01-06 17:55:55 -0500

    added YAML language support for Kubernetes manifests

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { "html", "cssls", "tsserver", "clangd", "terraformls", "yamlls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -17,4 +17,19 @@ lspconfig.pyright.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = {"python"},
+})
+
+-- Terraform LSP setup
+lspconfig.terraformls.setup({
+  cmd = { "terraform-ls", "serve" },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"terraform", "tf", "tfvars"},
+})
+
+-- YAML LSP setup
+lspconfig.yamlls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"yaml"},
 })
